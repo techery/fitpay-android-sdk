5669e114f77b9a2f6d290f523e7ff135d47eb7f2
Vladislav Yarovyi <skygo67@gmail.com>
2016-02-16 23:24:23 +0200

    generate SecretKey for JWE

---
+++ fitpay/src/main/java/com/fitpay/android/models/ECCKeyPair.java
@@ -0,0 +1,43 @@
+package com.fitpay.android.models;
+
+public class ECCKeyPair {
+    private String keyId;
+    private String publicKey;
+    private String clientPublicKey;
+    private transient String privateKey;
+
+    public ECCKeyPair(){
+    }
+
+    public String getKeyId() {
+        return keyId;
+    }
+
+    public void setKeyId(String keyId) {
+        this.keyId = keyId;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public String getClientPublicKey() {
+        return clientPublicKey;
+    }
+
+    public void setClientPublicKey(String clientPublicKey) {
+        this.clientPublicKey = clientPublicKey;
+    }
+
+    public String getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(String privateKey) {
+        this.privateKey = privateKey;
+    }
+}
--- testApplication/src/main/java/com/tabtale/myapplication/MainActivity.java
+++ testApplication/src/main/java/com/tabtale/myapplication/MainActivity.java
@@ -7,10 +7,28 @@ import android.util.Log;
 import com.fitpay.android.FitPay;
 import com.fitpay.android.api.FitPayService;
 import com.fitpay.android.api.oauth.OAuthConfig;
+import com.fitpay.android.models.ECCKeyPair;
 import com.fitpay.android.models.User;
 import com.fitpay.android.models.UsersCollection;
 import com.fitpay.android.units.APIUnit;
 import com.fitpay.android.utils.C;
+import com.fitpay.android.utils.KeyHandler;
+import com.nimbusds.jose.EncryptionMethod;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWEAlgorithm;
+import com.nimbusds.jose.JWEEncrypter;
+import com.nimbusds.jose.JWEHeader;
+import com.nimbusds.jose.JWEObject;
+import com.nimbusds.jose.KeyLengthException;
+import com.nimbusds.jose.Payload;
+import com.nimbusds.jose.crypto.AESDecrypter;
+import com.nimbusds.jose.crypto.AESEncrypter;
+import com.nimbusds.jose.crypto.bc.BouncyCastleProviderSingleton;
+
+import java.security.Security;
+import java.text.ParseException;
+
+import javax.crypto.SecretKey;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -26,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Security.addProvider(BouncyCastleProviderSingleton.getInstance());
+
+        testKeyPair();
+        //testFitPay();
+    }
+
+    private void testFitPay(){
         APIUnit api = new APIUnit(new OAuthConfig("e362a5cd-ab9d-4f9a-98ff-f91fcdd27936","s2CLUBKcbvQP6IqKx31XLclyqAd3nf6tyIPk74rL"));
         api.setAuthCallback(new APIUnit.IAuthCallback() {
             @Override
@@ -44,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
         fp.addUnit(api);
     }
 
-    public void getUsers(){
+    private void getUsers(){
         Call<UsersCollection> usersCall = fitPayAPI.getUsers(10, 0);
         usersCall.enqueue(new Callback<UsersCollection>() {
             @Override
@@ -61,12 +86,72 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void createUser(){
+    private void createUser(){
         User user = new User();
         user.setFirstName("John");
         user.setLastName("Doe");
         user.setBirthDate("1967-06-23");
         user.setEmail("john@doe.com");
+    }
 
+    private void testKeyPair(){
+        KeyHandler handler = new KeyHandler();
+        ECCKeyPair keyPairOne = null;
+        ECCKeyPair keyPairTwo = null;
+
+        try {
+            keyPairOne = handler.getKeyPair();
+            keyPairTwo = handler.getKeyPair();
+        } catch (Exception e) {
+        }
+
+        if(keyPairOne != null && keyPairTwo != null){
+
+            Log.i("KEY 1", "public: " + keyPairOne.getPublicKey());
+            Log.i("KEY 1", "private: " + keyPairOne.getPrivateKey());
+
+            Log.i("KEY 2", "public: " + keyPairTwo.getPublicKey());
+            Log.i("KEY 2", "private: " + keyPairTwo.getPublicKey());
+
+            SecretKey secretKey1 = handler.generateSharedSecret(keyPairOne.getPrivateKey(), keyPairTwo.getPublicKey());
+            SecretKey secretKey2 = handler.generateSharedSecret(keyPairTwo.getPrivateKey(), keyPairOne.getPublicKey());
+
+            String thisIsSparta = "This is SPARTAAAAA!!!";
+
+            JWEAlgorithm alg = JWEAlgorithm.A256GCMKW;
+            EncryptionMethod enc = EncryptionMethod.A256GCM;
+
+            JWEHeader.Builder jweHeaderBuilder = new JWEHeader.Builder(alg, enc);
+            JWEHeader header = jweHeaderBuilder.build();
+            Payload payload = new Payload(thisIsSparta);
+            JWEObject jweObject = new JWEObject(header, payload);
+            JWEEncrypter encrypter = null;
+            try {
+                encrypter = new AESEncrypter(secretKey1);
+                jweObject.encrypt(encrypter);
+            } catch (KeyLengthException e) {
+                e.printStackTrace();
+            } catch (JOSEException e) {
+                e.printStackTrace();
+            }
+
+            String jweHeader = jweObject.getHeader().toString();
+            String jwe = jweObject.serialize();
+
+            try {
+                jweObject = JWEObject.parse(jwe);
+                jweObject.decrypt(new AESDecrypter(secretKey2));
+            } catch (JOSEException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            String newString = jweObject.getPayload().toString();
+
+            if(thisIsSparta.equals(newString)){
+                Log.i("ENC_DEC", "SUCCESS");
+            }
+        }
     }
 }
