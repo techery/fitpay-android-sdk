50baf541043f4dc0026a317afaaaad41698e451b
rossgabay <rgabay@rgabay.com>
2016-05-03 17:58:59 -0400

    modified WebViewCommunicator to correspond to the latest developments on the webview side. added stub implementation.

---
+++ fitpay/src/main/java/com/fitpay/android/webview/impl/AckResponseModel.java
@@ -0,0 +1,14 @@
+package com.fitpay.android.webview.impl;
+
+
+public class AckResponseModel {
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
