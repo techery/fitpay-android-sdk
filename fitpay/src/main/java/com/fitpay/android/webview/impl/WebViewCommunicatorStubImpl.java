bcec0a4a62eb59bd54f7ad13d8268ad9e6ece754
rossgabay <rgabay@rgabay.com>
2016-05-13 16:51:58 -0400

    refactored web view communicator impl, added model for sync response

--- fitpay/src/main/java/com/fitpay/android/webview/impl/WebViewCommunicatorStubImpl.java
+++ fitpay/src/main/java/com/fitpay/android/webview/impl/WebViewCommunicatorStubImpl.java
@@ -2,11 +2,13 @@ package com.fitpay.android.webview.impl;
 
 import android.webkit.JavascriptInterface;
 
+import com.fitpay.android.R;
 import com.fitpay.android.webview.WebViewCommunicator;
 import  com.fitpay.android.webview.callback.OnTaskCompleted;
 
 import com.google.gson.Gson;
 import android.app.Activity;
+import android.webkit.WebView;
 
 
 /**
@@ -17,18 +19,21 @@ public class WebViewCommunicatorStubImpl implements WebViewCommunicator {
 
     private final Activity activity;
     private OnTaskCompleted callback;
+    private  int wId;
 
-    public WebViewCommunicatorStubImpl(Activity ctx, OnTaskCompleted callback) {
+    public WebViewCommunicatorStubImpl(Activity ctx, int wId, OnTaskCompleted callback) {
         this.activity = ctx;
+        this.wId = wId;
         this.callback = callback;
     }
 
-    public WebViewCommunicatorStubImpl(Activity ctx) {
+    public WebViewCommunicatorStubImpl(Activity ctx, int wId) {
         this.activity = ctx;
+        this.wId = wId;
     }
 
     private static String USER_DATA_STUB_RESPONSE = "OK";
-    private static String SYNC_STUB_RESPONSE = "OK";
+    private static String SYNC_STUB_RESPONSE = "0";
 
     final Gson gson = new Gson();
 
@@ -38,13 +43,29 @@ public class WebViewCommunicatorStubImpl implements WebViewCommunicator {
         AckResponseModel stubResponse = new AckResponseModel();
         stubResponse.setStatus(USER_DATA_STUB_RESPONSE);
 
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        SyncResponseModel syncResponse = new SyncResponseModel();
 
-        if(callback!=null) callback.onTaskCompleted(SYNC_STUB_RESPONSE);
+           Runnable t = new Runnable(){
+                @Override
+                public void run(){
+                    try{
+                        //this is where the "underlying" SDK's sync is called
+                        Thread.sleep(5000);
+
+                        syncResponse.setStatus(SYNC_STUB_RESPONSE);
+                        sendMessageToJs(gson.toJson(syncResponse));
+                        if(callback!=null) callback.onTaskCompleted(SYNC_STUB_RESPONSE);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+        Thread thread = new Thread(t);
+        thread.start();
+
+
         return gson.toJson(stubResponse);
     }
 
@@ -63,6 +84,17 @@ public class WebViewCommunicatorStubImpl implements WebViewCommunicator {
     @JavascriptInterface
     public String retrieveConfigJson() {
         throw new UnsupportedOperationException("method not supported in this iteration");
+    }
+
+    public void sendMessageToJs(String msg) {
+        final String str = msg;
+        WebView w = (WebView)activity.findViewById(wId);
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                w.loadUrl("javascript:droid_callback('" + msg+ "');");
+            }
+        });
     }
 }
 
