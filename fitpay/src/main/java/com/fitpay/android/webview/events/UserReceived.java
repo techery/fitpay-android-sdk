39109b7fb204fa105bb5d7f28176f7992ecda0c2
Vladislav Yarovyi <skygo67@gmail.com>
2016-11-15 18:43:10 +0200

    onUserReceived: send userID

--- fitpay/src/main/java/com/fitpay/android/webview/events/UserReceived.java
+++ fitpay/src/main/java/com/fitpay/android/webview/events/UserReceived.java
@@ -2,10 +2,16 @@ package com.fitpay.android.webview.events;
 
 public class UserReceived {
 
+    private String userId;
     private String email;
 
-    public UserReceived(String email) {
+    public UserReceived(String userId, String email) {
+        this.userId = userId;
         this.email = email;
+    }
+
+    public String userId() {
+        return userId;
     }
 
     public String getEmail() {
