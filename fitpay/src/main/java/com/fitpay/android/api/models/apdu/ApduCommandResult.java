7eed22ab0a91894970fb6f89945b94db7c754461
Vladislav Yarovyi <skygo67@gmail.com>
2016-11-16 19:12:16 +0200

    ApduExecutionResult with continueOnFailure (transient)

--- fitpay/src/main/java/com/fitpay/android/api/models/apdu/ApduCommandResult.java
+++ fitpay/src/main/java/com/fitpay/android/api/models/apdu/ApduCommandResult.java
@@ -8,8 +8,10 @@ public class ApduCommandResult {
     private String commandId;
     private String responseCode;
     private String responseData;
+    private transient boolean continueOnFailure;
 
-    private ApduCommandResult() {}
+    private ApduCommandResult() {
+    }
 
     public String getCommandId() {
         return commandId;
@@ -23,18 +25,25 @@ public class ApduCommandResult {
         return responseCode;
     }
 
+    public boolean canContinueOnFailure() {
+        return continueOnFailure;
+    }
+
     public static class Builder {
         private String commandId;
         private String responseCode;
         private String responseData;
+        private boolean continueOnFailure;
 
-        public Builder() {}
+        public Builder() {
+        }
 
         public ApduCommandResult build() {
             ApduCommandResult result = new ApduCommandResult();
             result.commandId = this.commandId;
             result.responseCode = this.responseCode;
             result.responseData = this.responseData;
+            result.continueOnFailure = this.continueOnFailure;
             return result;
         }
 
@@ -50,6 +59,11 @@ public class ApduCommandResult {
 
         public Builder setResponseData(String responseData) {
             this.responseData = responseData;
+            return this;
+        }
+
+        public Builder setContinueOnFailure(boolean continueOnFailure) {
+            this.continueOnFailure = continueOnFailure;
             return this;
         }
     }
