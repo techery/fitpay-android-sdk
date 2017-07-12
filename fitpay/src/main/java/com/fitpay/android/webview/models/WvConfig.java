31ad33749969212bbe9fb9561e57e4b330adbe7f
Vladislav Yarovyi <skygo67@gmail.com>
2017-07-12 22:26:52 +0300

    move wvconfig from app to sdk. deviceInfo, deviceInfoSecure

---
+++ fitpay/src/main/java/com/fitpay/android/webview/models/WvConfig.java
@@ -0,0 +1,86 @@
+package com.fitpay.android.webview.models;
+
+import android.util.Base64;
+
+import com.google.gson.Gson;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+/**
+ * Encoded wv config
+ */
+public class WvConfig {
+
+    private HashMap<String, Object> data = new HashMap<>();
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(data);
+    }
+
+    public String getEncodedString() {
+        byte[] bytesToEncode = toString().getBytes(StandardCharsets.UTF_8);
+        return Base64.encodeToString(bytesToEncode, Base64.URL_SAFE);
+    }
+
+    public static class Builder<T extends WvPaymentDeviceInfo> {
+        private HashMap<String, Object> data = new HashMap<>();
+
+        public Builder accountExist(boolean value) {
+            data.put("accountExist", value);
+            return this;
+        }
+
+        public Builder demoMode(boolean value) {
+            data.put("demoMode", value);
+            return this;
+        }
+
+        public Builder demoCardGroup(String demoCardGroup) {
+            data.put("demoCardGroup", demoCardGroup);
+            return this;
+        }
+
+        public Builder email(String email) {
+            data.put("email", email);
+            return this;
+        }
+
+        public Builder clientId(String clientId) {
+            data.put("clientId", clientId);
+            return this;
+        }
+
+        public Builder version(String version) {
+            data.put("version", version);
+            return this;
+        }
+
+        public Builder paymentDevice(T paymentDevice) {
+            data.put("paymentDevice", paymentDevice);
+            return this;
+        }
+
+        public Builder redirectUri(String redirectUri) {
+            data.put("redirectUri", redirectUri);
+            return this;
+        }
+
+        public Builder setCSSUrl(String cssUrl) {
+            data.put("themeOverrideCssUrl", cssUrl);
+            return this;
+        }
+
+        public Builder addKeyValue(String key, Object value) {
+            data.put(key, value);
+            return this;
+        }
+
+        public WvConfig build() {
+            WvConfig wvConfig = new WvConfig();
+            wvConfig.data = data;
+            return wvConfig;
+        }
+    }
+}
