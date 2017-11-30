34f87450a70706dc0258555ad4553e15f9f13f4c
Sara Montecino <sara@fit-pay.com>
2017-11-29 18:38:43 -0700

    Fix bug where UnrecognizedRtmMessage infinitely put on bus

--- fitpay/src/main/java/com/fitpay/android/webview/events/UnrecognizedRtmMessage.java
+++ fitpay/src/main/java/com/fitpay/android/webview/events/UnrecognizedRtmMessage.java
@@ -4,8 +4,26 @@ package com.fitpay.android.webview.events;
  * Unrecognized {@link RtmMessage}
  */
 
-public class UnrecognizedRtmMessage extends RtmMessage {
+public class UnrecognizedRtmMessage {
+    private final String type;
+    private final String data;
+    private final String callbackId;
+
     public UnrecognizedRtmMessage(RtmMessage msg) {
-        super(msg.getCallbackId(), msg.getJsonData(), msg.getType());
+        this.type = msg.getType();
+        this.data = msg.getData();
+        this.callbackId = msg.getCallbackId();
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public String getData(){
+        return this.data;
+    }
+
+    public String getCallbackId(){
+        return this.callbackId;
     }
 }
