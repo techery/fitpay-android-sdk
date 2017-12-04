a70c533d8bad772eebbd6cd1dc3b9e367af1e628
Vladislav Yarovyi <skygo67@gmail.com>
2017-12-05 00:57:41 +0200

    rx update. add some logs into test

--- fitpay/src/test/java/com/fitpay/android/webview/impl/UnrecognizedMessageTest.java
+++ fitpay/src/test/java/com/fitpay/android/webview/impl/UnrecognizedMessageTest.java
@@ -1,5 +1,7 @@
 package com.fitpay.android.webview.impl;
 
+import com.fitpay.android.BearerTokenTest;
+import com.fitpay.android.TestActions;
 import com.fitpay.android.utils.Constants;
 import com.fitpay.android.utils.FPLog;
 import com.fitpay.android.utils.Listener;
@@ -7,6 +9,8 @@ import com.fitpay.android.utils.NotificationManager;
 import com.fitpay.android.utils.RxBus;
 import com.fitpay.android.webview.events.RtmMessage;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.concurrent.CountDownLatch;
@@ -22,28 +26,42 @@ import static junit.framework.Assert.assertNotNull;
  * Created by Vlad on 04.12.2017.
  */
 
-public class UnrecognizedMessageTest {
+public class UnrecognizedMessageTest extends TestActions{
 
     private RtmMessage message;
 
+    private CountDownLatch latch;
+    private UnrecognizedRtmMessageListener listener;
+
+    @Before
+    @Override
+    public void testActionsSetup() throws Exception {
+        latch = new CountDownLatch(1);
+        this.listener = new UnrecognizedRtmMessageListener(latch);
+        NotificationManager.getInstance().addListener(listener, Schedulers.immediate());
+    }
+
+    @After
+    public void cleanup() {
+        NotificationManager.getInstance().removeListener(listener);
+        this.listener = null;
+    }
+
     @Test
     public void testUnrecognizedRtmMessage() {
-        final CountDownLatch latch = new CountDownLatch(1);
-        UnrecognizedRtmMessageListener listener = new UnrecognizedRtmMessageListener(latch);
-        NotificationManager.getInstance().addListener(listener, Schedulers.immediate());
-
         String rtmMsgStr = "{\"callbackId\":\"10\",\"data\":\"{\\\"resource\\\":\\\"The Truth Is Out There\\\"}\",\"type\":\"somethingUnknown\"}";
         RtmMessage msg = Constants.getGson().fromJson(rtmMsgStr, RtmMessage.class);
 
         RxBus.getInstance().post(msg);
 
         try {
+            FPLog.d("UnrecognizedMessageTest", "start:" + System.currentTimeMillis());
             latch.await(30, TimeUnit.SECONDS);
+            FPLog.d("UnrecognizedMessageTest", "end:" + System.currentTimeMillis());
         } catch (InterruptedException e) {
+            FPLog.d("UnrecognizedMessageTest", "exception:" + System.currentTimeMillis());
             e.printStackTrace();
         }
-
-        NotificationManager.getInstance().removeListener(listener);
 
         assertNotNull("unrecognized message shouldn't be null", message);
         UnrecognizedRtmData data = Constants.getGson().fromJson(message.getJsonData(), UnrecognizedRtmData.class);
