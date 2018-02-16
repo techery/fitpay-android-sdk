b2080503a727e65be6bf50b395bde8a89e09b9c7
Vlad <skygo67@gmail.com>
2018-02-16 21:53:22 +0200

    Update TestConstants.java
    remove test clientId.

--- fitpay/src/test/java/com/fitpay/android/TestConstants.java
+++ fitpay/src/test/java/com/fitpay/android/TestConstants.java
@@ -14,7 +14,7 @@ public final class TestConstants {
         Map<String, String> config = new HashMap<>();
         config.put(ApiManager.PROPERTY_API_BASE_URL, System.getProperty(ApiManager.PROPERTY_API_BASE_URL, "https://api.fit-pay.com"));
         config.put(ApiManager.PROPERTY_AUTH_BASE_URL, System.getProperty(ApiManager.PROPERTY_AUTH_BASE_URL, "https://auth.fit-pay.com"));
-        config.put(ApiManager.PROPERTY_CLIENT_ID, System.getProperty(ApiManager.PROPERTY_CLIENT_ID, "pagare"));//"fp_webapp_pJkVp2Rl"));
+        config.put(ApiManager.PROPERTY_CLIENT_ID, System.getProperty(ApiManager.PROPERTY_CLIENT_ID, "fp_webapp_pJkVp2Rl"));
         config.put(ApiManager.PROPERTY_REDIRECT_URI, System.getProperty(ApiManager.PROPERTY_REDIRECT_URI, "https://webapp.fit-pay.com"));
 
         System.out.println("test configuration: " + config);
